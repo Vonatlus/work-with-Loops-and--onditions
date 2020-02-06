375a34dd9dd9d12f93fb35cc013e6619abe98b50
Sultanov Aleksandre <sultanov.aleksandre@gmail.com>
2020-02-07 01:56:39 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+let numberN = +prompt(`Enter an number N`, 10);
+
+while (!Number.isInteger(numberN)) {
+  numberN = +prompt(`Enter the whole number N`, 10);
+}
+console.log(`Number N: `, numberN);
+document.writeln(`<b>Number N:</b> ${numberN}<br>`);
+
+let numberM = +prompt(`Enter an number M`, 100);
+while (!Number.isInteger(numberM) || numberN >= numberM) {
+  numberM = +prompt(
+    `Enter the number which is whole and biggest than number N`,
+    100
+  );
+}
+console.log(`Number M: `, numberM);
+document.writeln(`<b>Number M:</b> ${numberM} <br>`);
+
+let ignoreEven = confirm(`Ignore even number?`);
+console.log(`Ignore even number?:`, ignoreEven);
+document.writeln(`<b>Ignore even number?:</b> ${ignoreEven} <br>`);
+
+let conclusion = 0;
+
+for (numberN; numberN <= numberM; numberN++) {
+  if (ignoreEven === true) {
+    if (numberN % 2 !== 0) {
+      conclusion += numberN;
+    }
+  } else {
+    conclusion += numberN;
+  }
+}
+
+console.log(`Conclusion:`, conclusion);
+document.writeln(`<b>Conclusion:</b> ${conclusion} <br>`);
